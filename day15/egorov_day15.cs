83b397c29c8873a27ffd6e03f7f7eed58a57a640
Pavel Egorov <xoposhiy@mail.ru>
2017-12-15 10:21:02 +0500

    15

---
+++ day15/egorov_day15.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AdventOfCode2017
+{
+    [TestFixture]
+    public static class Problem
+    {
+        [Test]
+        public static void RunSolve()
+        {
+            Solve(65, 8921);
+            Solve(722, 354);
+        }
+
+        public static void Solve(int aStart, int bStart)
+        {
+            var count = Gen(aStart, 16807, 4)
+                .Zip(
+                    Gen(bStart, 48271, 8), 
+                    (a, b) => ((a^b) << 16) == 0)
+                .Take(5000000)
+                .Count(ok => ok);
+            Console.WriteLine(count);
+        }
+        
+        public static IEnumerable<int> Gen(int start, int mult, int check)
+        {
+            while(true)
+            {
+                start = (int)(((long)start * mult) % 2147483647);
+                if (start % check == 0) yield return start;
+            }
+        }
+    }
+}
